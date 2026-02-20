278ab5ce7719db1b144d170a0b3a0db3494dd78f
Siddhi Pandya <siddhi.pandya@armakuni.com>
2026-02-20 15:17:50 +0530

    fix: AT3-A2672 - Update in Card Dictionary

--- card.py
+++ card.py
@@ -2,12 +2,12 @@ class Card:
     VALUES={
         "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11 
     }
-    def __int__(self, suit, rank):
-        self.suit = suit
+    def __int__(self, rank, suit):
         self.rank = rank
-
-    def value(self):
-        return self.VALUES[self.rank]
+        self.suit = suit
+        
+    def get_value(self):
+        return Card.VALUES[self.rank]
 
     def __str__(self):
         return self.rank + self.suit
